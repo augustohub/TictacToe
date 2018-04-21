ded90d949f6ff6aa229dbeced7bb01e2954153b0
Augusto Hubert <augustohub@gmail.com>
2018-04-20 23:59:01 -0300

    Creates Spot class

--- lib/TictacToe.rb
+++ lib/TictacToe.rb
@@ -1,6 +1,7 @@
 require "TictacToe/version"
 require_relative "./TictacToe/player.rb"
 require_relative "./TictacToe/bot.rb"
+require_relative "./TictacToe/spot.rb"
 
 module TictacToe
   # Your code goes here...
