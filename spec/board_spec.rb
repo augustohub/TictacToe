ff2ded97fa2f70ed1b6ae4018bb32ff1ee7b98f1
Augusto Hubert <augustohub@gmail.com>
2018-04-21 01:04:00 -0300

    creates new board test specs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,7 +5,16 @@ module TictacToe
     context '#initialize' do
       it 'creates a board with empty spots' do
         board = Board.new
-        expect(board).to have_exactly(9).items
+        expect(board.spots.size).to eql(9)
+        expect(board.available_spots.size).to eql(9)
+      end
+    end
+
+    context 'gets the marker of an spot' do
+      it 'returns the marker based on his index' do
+        board = Board.new
+        board.spot(3).value = "X"
+        expect(board.spot_value(3)).to eql("X")
       end
     end
   end
