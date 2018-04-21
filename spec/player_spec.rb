0f35d02e1b59c0aebdd43e9e41fbaf7f2b58801a
Augusto Hubert <augustohub@gmail.com>
2018-04-20 23:23:02 -0300

    Add explicit module declaration on player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
-RSpec.describe "Player" do
-  context '#initialize' do
-    it 'initializes with correct attributes' do
-      attrs = { name: 'Player1', marker: 'X' }
-      expect { Player.new(attrs).to_not raise_error }
+module TictacToe
+  RSpec.describe Player do
+    context '#initialize' do
+      it 'initializes with correct attributes' do
+        name = 'Player1'
+        expect { Player.new(name).to_not raise_error }
+      end
+
+      it 'raises an error initializes without correct attributes' do
+        expect { Player.new }.to raise_error(ArgumentError)
+      end
     end
   end
 end
